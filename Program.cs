9112c01cbeb6a0988d4f30a78bef90213b4fa223
Miguel-Correia <miguelncorreia99@gmail.com>
2025-06-01 14:36:26 +0100

    Added materializecss

--- Program.cs
+++ Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Movies}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
