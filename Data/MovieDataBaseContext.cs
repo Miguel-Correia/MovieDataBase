cb645948e30bf0f2eed5a61fbb54e8518fbed9d5
Miguel <miguelncorreia99@gmail.com>
2025-10-30 19:33:52 +0000

    Added more seed migrations

--- Data/MovieDataBaseContext.cs
+++ Data/MovieDataBaseContext.cs
@@ -339,48 +339,246 @@ namespace MovieDataBase.Data
 				new Role { Id = 3, Name = "Writer" },
 				new Role { Id = 4, Name = "Producer" }
 			);
-			
+
 			// --- SEED PEOPLE-ROLES-IN-MOVIES (Tabela de Junção) ---
 			modelBuilder.Entity<PeopleRolesInMovies>().HasData(
-			    // The Dark Knight (MovieId: 1)
-			    new { MovieId = 1, PeopleId = 1, RoleId = 1, CharacterName = (string?)null },  // Christopher Nolan - Director
-			    new { MovieId = 1, PeopleId = 7, RoleId = 2, CharacterName = "Bruce Wayne / Batman" },  // Christian Bale - Actor
-			    new { MovieId = 1, PeopleId = 8, RoleId = 2, CharacterName = "The Joker" },  // Heath Ledger - Actor
-			    new { MovieId = 1, PeopleId = 9, RoleId = 2, CharacterName = "Lucius Fox" },  // Morgan Freeman - Actor
+				// The Dark Knight (MovieId: 1)
+				new { MovieId = 1, PeopleId = 1, RoleId = 1, CharacterName = (string?)null },  // Christopher Nolan - Director
+				new { MovieId = 1, PeopleId = 7, RoleId = 2, CharacterName = "Bruce Wayne / Batman" },  // Christian Bale - Actor
+				new { MovieId = 1, PeopleId = 8, RoleId = 2, CharacterName = "The Joker" },  // Heath Ledger - Actor
+				new { MovieId = 1, PeopleId = 9, RoleId = 2, CharacterName = "Lucius Fox" },  // Morgan Freeman - Actor
+
+				// The Shawshank Redemption (MovieId: 2)
+				new { MovieId = 2, PeopleId = 2, RoleId = 1, CharacterName = (string?)null },  // Frank Darabont - Director
+				new { MovieId = 2, PeopleId = 10, RoleId = 2, CharacterName = "Andy Dufresne" },  // Tim Robbins - Actor
+				new { MovieId = 2, PeopleId = 9, RoleId = 2, CharacterName = "Ellis Boyd 'Red' Redding" },  // Morgan Freeman - Actor
+
+				// Superbad (MovieId: 3)
+				new { MovieId = 3, PeopleId = 3, RoleId = 1, CharacterName = (string?)null },  // Greg Mottola - Director
+				new { MovieId = 3, PeopleId = 11, RoleId = 2, CharacterName = "Seth" },  // Jonah Hill - Actor
+				new { MovieId = 3, PeopleId = 12, RoleId = 2, CharacterName = "Evan" },  // Michael Cera - Actor
+
+				// Inception (MovieId: 4)
+				new { MovieId = 4, PeopleId = 1, RoleId = 1, CharacterName = (string?)null },  // Christopher Nolan - Director
+				new { MovieId = 4, PeopleId = 13, RoleId = 2, CharacterName = "Dom Cobb" },  // Leonardo DiCaprio - Actor
+				new { MovieId = 4, PeopleId = 14, RoleId = 2, CharacterName = "Eames" },  // Tom Hardy - Actor
+				new { MovieId = 4, PeopleId = 15, RoleId = 2, CharacterName = "Ariadne" },  // Elliot Page - Actor
+
+				// Forrest Gump (MovieId: 5)
+				new { MovieId = 5, PeopleId = 4, RoleId = 1, CharacterName = (string?)null },  // Robert Zemeckis - Director
+				new { MovieId = 5, PeopleId = 16, RoleId = 2, CharacterName = "Forrest Gump" },  // Tom Hanks - Actor
+
+				// Mad Max: Fury Road (MovieId: 6)
+				new { MovieId = 6, PeopleId = 5, RoleId = 1, CharacterName = (string?)null },  // George Miller - Director
+				new { MovieId = 6, PeopleId = 14, RoleId = 2, CharacterName = "Max Rockatansky" },  // Tom Hardy - Actor
+				new { MovieId = 6, PeopleId = 17, RoleId = 2, CharacterName = "Imperator Furiosa" },  // Charlize Theron - Actor
+
+				// Interstellar (MovieId: 7)
+				new { MovieId = 7, PeopleId = 1, RoleId = 1, CharacterName = (string?)null },  // Christopher Nolan - Director
+				new { MovieId = 7, PeopleId = 18, RoleId = 2, CharacterName = "Cooper" },  // Matthew McConaughey - Actor
+				new { MovieId = 7, PeopleId = 19, RoleId = 2, CharacterName = "Brand" },  // Anne Hathaway - Actor
+
+				// Parasite (MovieId: 8)
+				new { MovieId = 8, PeopleId = 6, RoleId = 1, CharacterName = (string?)null },  // Bong Joon-ho - Director
+				new { MovieId = 8, PeopleId = 20, RoleId = 2, CharacterName = "Kim Ki-taek" }  // Song Kang-ho - Actor
+			);
 			
-			    // The Shawshank Redemption (MovieId: 2)
-			    new { MovieId = 2, PeopleId = 2, RoleId = 1, CharacterName = (string?)null },  // Frank Darabont - Director
-			    new { MovieId = 2, PeopleId = 10, RoleId = 2, CharacterName = "Andy Dufresne" },  // Tim Robbins - Actor
-			    new { MovieId = 2, PeopleId = 9, RoleId = 2, CharacterName = "Ellis Boyd 'Red' Redding" },  // Morgan Freeman - Actor
+			// --- SEED PEOPLE (adicionar aos existentes) ---
+			modelBuilder.Entity<People>().HasData(
 			
-			    // Superbad (MovieId: 3)
-			    new { MovieId = 3, PeopleId = 3, RoleId = 1, CharacterName = (string?)null },  // Greg Mottola - Director
-			    new { MovieId = 3, PeopleId = 11, RoleId = 2, CharacterName = "Seth" },  // Jonah Hill - Actor
-			    new { MovieId = 3, PeopleId = 12, RoleId = 2, CharacterName = "Evan" },  // Michael Cera - Actor
-			
-			    // Inception (MovieId: 4)
-			    new { MovieId = 4, PeopleId = 1, RoleId = 1, CharacterName = (string?)null },  // Christopher Nolan - Director
-			    new { MovieId = 4, PeopleId = 13, RoleId = 2, CharacterName = "Dom Cobb" },  // Leonardo DiCaprio - Actor
-			    new { MovieId = 4, PeopleId = 14, RoleId = 2, CharacterName = "Eames" },  // Tom Hardy - Actor
-			    new { MovieId = 4, PeopleId = 15, RoleId = 2, CharacterName = "Ariadne" },  // Elliot Page - Actor
-			
-			    // Forrest Gump (MovieId: 5)
-			    new { MovieId = 5, PeopleId = 4, RoleId = 1, CharacterName = (string?)null },  // Robert Zemeckis - Director
-			    new { MovieId = 5, PeopleId = 16, RoleId = 2, CharacterName = "Forrest Gump" },  // Tom Hanks - Actor
-			
-			    // Mad Max: Fury Road (MovieId: 6)
-			    new { MovieId = 6, PeopleId = 5, RoleId = 1, CharacterName = (string?)null },  // George Miller - Director
-			    new { MovieId = 6, PeopleId = 14, RoleId = 2, CharacterName = "Max Rockatansky" },  // Tom Hardy - Actor
-			    new { MovieId = 6, PeopleId = 17, RoleId = 2, CharacterName = "Imperator Furiosa" },  // Charlize Theron - Actor
-			
-			    // Interstellar (MovieId: 7)
-			    new { MovieId = 7, PeopleId = 1, RoleId = 1, CharacterName = (string?)null },  // Christopher Nolan - Director
-			    new { MovieId = 7, PeopleId = 18, RoleId = 2, CharacterName = "Cooper" },  // Matthew McConaughey - Actor
-			    new { MovieId = 7, PeopleId = 19, RoleId = 2, CharacterName = "Brand" },  // Anne Hathaway - Actor
-			
-			    // Parasite (MovieId: 8)
-			    new { MovieId = 8, PeopleId = 6, RoleId = 1, CharacterName = (string?)null },  // Bong Joon-ho - Director
-			    new { MovieId = 8, PeopleId = 20, RoleId = 2, CharacterName = "Kim Ki-taek" }  // Song Kang-ho - Actor
+			    // Escritores
+			    new People 
+			    { 
+			        Id = 21, 
+			        Name = "Jonathan Nolan", 
+			        DateOfBirth = new DateTime(1976, 6, 6),
+			        Biography = "British-American screenwriter and producer, brother of Christopher Nolan. Co-wrote The Dark Knight trilogy, Interstellar, and created Westworld."
+			    },
+			    new People 
+			    { 
+			        Id = 22, 
+			        Name = "David Goyer", 
+			        DateOfBirth = new DateTime(1965, 12, 22),
+			        Biography = "American screenwriter, film director and comic book writer. Known for writing Blade trilogy and The Dark Knight trilogy."
+			    },
+			    new People 
+			    { 
+			        Id = 23, 
+			        Name = "Stephen King", 
+			        DateOfBirth = new DateTime(1947, 9, 21),
+			        Biography = "American author known as the 'King of Horror'. Wrote the novella 'Rita Hayworth and Shawshank Redemption' which inspired the film."
+			    },
+			    new People 
+			    { 
+			        Id = 24, 
+			        Name = "Seth Rogen", 
+			        DateOfBirth = new DateTime(1982, 4, 15),
+			        Biography = "Canadian-American actor, comedian, and writer. Co-wrote Superbad with Evan Goldberg based on their teenage experiences."
+			    },
+			    new People 
+			    { 
+			        Id = 25, 
+			        Name = "Evan Goldberg", 
+			        DateOfBirth = new DateTime(1982, 9, 11),
+			        Biography = "Canadian screenwriter and producer. Frequent collaborator with Seth Rogen on films like Superbad and Pineapple Express."
+			    },
+			    new People 
+			    { 
+			        Id = 26, 
+			        Name = "Eric Roth", 
+			        DateOfBirth = new DateTime(1945, 3, 22),
+			        Biography = "American screenwriter. Academy Award winner for Forrest Gump. Also wrote Munich, The Curious Case of Benjamin Button, and Dune."
+			    },
+			    new People 
+			    { 
+			        Id = 27, 
+			        Name = "Winston Groom", 
+			        DateOfBirth = new DateTime(1943, 3, 23),
+			        DateOfDeath = new DateTime(2020, 9, 17),
+			        Biography = "American novelist and non-fiction writer. Wrote the novel 'Forrest Gump' which was adapted into the Oscar-winning film."
+			    },
+			    new People 
+			    { 
+			        Id = 28, 
+			        Name = "Brendan McCarthy", 
+			        DateOfBirth = new DateTime(1957, 3, 28),
+			        Biography = "British artist and designer. Co-wrote Mad Max: Fury Road and designed much of its distinctive visual style."
+			    },
+			    new People 
+			    { 
+			        Id = 29, 
+			        Name = "Han Jin-won", 
+			        DateOfBirth = new DateTime(1989, 1, 1),
+			        Biography = "South Korean screenwriter. Co-wrote Parasite with Bong Joon-ho, winning the Academy Award for Best Original Screenplay."
+			    },
+
+			    // Produtores
+			    new People 
+			    { 
+			        Id = 30, 
+			        Name = "Emma Thomas", 
+			        DateOfBirth = new DateTime(1971, 12, 9),
+			        Biography = "British film producer and wife of Christopher Nolan. Produced all of Nolan's films including The Dark Knight trilogy, Inception, and Interstellar."
+			    },
+			    new People 
+			    { 
+			        Id = 31, 
+			        Name = "Charles Roven", 
+			        DateOfBirth = new DateTime(1949, 8, 2),
+			        Biography = "American film producer. Produced The Dark Knight trilogy, Man of Steel, and numerous other blockbusters."
+			    },
+			    new People 
+			    { 
+			        Id = 32, 
+			        Name = "Niki Marvin", 
+			        DateOfBirth = new DateTime(1952, 6, 8),
+			        Biography = "American film producer. Worked on The Shawshank Redemption and The Majestic with Frank Darabont."
+			    },
+			    new People 
+			    { 
+			        Id = 33, 
+			        Name = "Judd Apatow", 
+			        DateOfBirth = new DateTime(1967, 12, 6),
+			        Biography = "American producer, director, and comedian. Produced Superbad and numerous other comedy hits like Knocked Up and The 40-Year-Old Virgin."
+			    },
+			    new People 
+			    { 
+			        Id = 34, 
+			        Name = "Shaun McKittrick", 
+			        DateOfBirth = new DateTime(1968, 5, 15),
+			        Biography = "American film producer. Produced Superbad and other Judd Apatow productions."
+			    },
+			    new People 
+			    { 
+			        Id = 35, 
+			        Name = "Wendy Finerman", 
+			        DateOfBirth = new DateTime(1960, 1, 1),
+			        Biography = "American film producer. Won Academy Award for producing Forrest Gump. Also produced The Devil Wears Prada."
+			    },
+			    new People 
+			    { 
+			        Id = 36, 
+			        Name = "Steve Tisch", 
+			        DateOfBirth = new DateTime(1949, 2, 14),
+			        Biography = "American film producer and businessman. Co-produced Forrest Gump. Also co-owner of the New York Giants NFL team."
+			    },
+			    new People 
+			    { 
+			        Id = 37, 
+			        Name = "Doug Mitchell", 
+			        DateOfBirth = new DateTime(1952, 10, 15),
+			        Biography = "Australian film producer. Long-time collaborator with George Miller on Mad Max films and Babe."
+			    },
+			    new People 
+			    { 
+			        Id = 38, 
+			        Name = "Lynda Obst", 
+			        DateOfBirth = new DateTime(1950, 4, 14),
+			        Biography = "American film producer. Produced Interstellar, Contact, and Sleepless in Seattle."
+			    },
+			    new People 
+			    { 
+			        Id = 39, 
+			        Name = "Kwak Sin-ae", 
+			        DateOfBirth = new DateTime(1968, 3, 12),
+			        Biography = "South Korean film producer. Produced Parasite and The Handmaiden. President of Barunson E&A."
+			    },
+			    new People 
+			    { 
+			        Id = 40, 
+			        Name = "Jang Young-hwan", 
+			        DateOfBirth = new DateTime(1975, 5, 20),
+			        Biography = "South Korean film producer. Co-produced Parasite with Kwak Sin-ae."
+			    }
+			);
+
+			// --- SEED PEOPLE-ROLES-IN-MOVIES (adicionar aos existentes) ---
+			modelBuilder.Entity<PeopleRolesInMovies>().HasData(
+		    	// ... (mantém todos os anteriores)
+
+		    	// The Dark Knight - Writers & Producers
+		    	new { MovieId = 1, PeopleId = 21, RoleId = 3, CharacterName = (string?)null },  // Jonathan Nolan - Writer
+		    	new { MovieId = 1, PeopleId = 22, RoleId = 3, CharacterName = (string?)null },  // David Goyer - Writer
+		    	new { MovieId = 1, PeopleId = 30, RoleId = 4, CharacterName = (string?)null },  // Emma Thomas - Producer
+		    	new { MovieId = 1, PeopleId = 31, RoleId = 4, CharacterName = (string?)null },  // Charles Roven - Producer
+
+		    	// The Shawshank Redemption - Writers & Producers
+		    	new { MovieId = 2, PeopleId = 23, RoleId = 3, CharacterName = (string?)null },  // Stephen King - Writer (original story)
+		    	new { MovieId = 2, PeopleId = 2, RoleId = 3, CharacterName = (string?)null },   // Frank Darabont - Writer (screenplay)
+		    	new { MovieId = 2, PeopleId = 32, RoleId = 4, CharacterName = (string?)null },  // Niki Marvin - Producer
+
+		    	// Superbad - Writers & Producers
+		    	new { MovieId = 3, PeopleId = 24, RoleId = 3, CharacterName = (string?)null },  // Seth Rogen - Writer
+		    	new { MovieId = 3, PeopleId = 25, RoleId = 3, CharacterName = (string?)null },  // Evan Goldberg - Writer
+		    	new { MovieId = 3, PeopleId = 33, RoleId = 4, CharacterName = (string?)null },  // Judd Apatow - Producer
+		    	new { MovieId = 3, PeopleId = 34, RoleId = 4, CharacterName = (string?)null },  // Shaun McKittrick - Producer
+
+		    	// Inception - Writers & Producers
+		    	new { MovieId = 4, PeopleId = 1, RoleId = 3, CharacterName = (string?)null },   // Christopher Nolan - Writer
+		    	new { MovieId = 4, PeopleId = 30, RoleId = 4, CharacterName = (string?)null },  // Emma Thomas - Producer
+
+		    	// Forrest Gump - Writers & Producers
+		    	new { MovieId = 5, PeopleId = 26, RoleId = 3, CharacterName = (string?)null },  // Eric Roth - Writer (screenplay)
+		    	new { MovieId = 5, PeopleId = 27, RoleId = 3, CharacterName = (string?)null },  // Winston Groom - Writer (novel)
+			    new { MovieId = 5, PeopleId = 35, RoleId = 4, CharacterName = (string?)null },  // Wendy Finerman - Producer
+			    new { MovieId = 5, PeopleId = 36, RoleId = 4, CharacterName = (string?)null },  // Steve Tisch - Producer
+
+			    // Mad Max: Fury Road - Writers & Producers
+			    new { MovieId = 6, PeopleId = 5, RoleId = 3, CharacterName = (string?)null },   // George Miller - Writer
+			    new { MovieId = 6, PeopleId = 28, RoleId = 3, CharacterName = (string?)null },  // Brendan McCarthy - Writer
+			    new { MovieId = 6, PeopleId = 37, RoleId = 4, CharacterName = (string?)null },  // Doug Mitchell - Producer
+
+			    // Interstellar - Writers & Producers
+			    new { MovieId = 7, PeopleId = 1, RoleId = 3, CharacterName = (string?)null },   // Christopher Nolan - Writer
+			    new { MovieId = 7, PeopleId = 21, RoleId = 3, CharacterName = (string?)null },  // Jonathan Nolan - Writer
+			    new { MovieId = 7, PeopleId = 30, RoleId = 4, CharacterName = (string?)null },  // Emma Thomas - Producer
+			    new { MovieId = 7, PeopleId = 38, RoleId = 4, CharacterName = (string?)null },  // Lynda Obst - Producer
+
+			    // Parasite - Writers & Producers
+			    new { MovieId = 8, PeopleId = 6, RoleId = 3, CharacterName = (string?)null },   // Bong Joon-ho - Writer
+			    new { MovieId = 8, PeopleId = 29, RoleId = 3, CharacterName = (string?)null },  // Han Jin-won - Writer
+			    new { MovieId = 8, PeopleId = 39, RoleId = 4, CharacterName = (string?)null },  // Kwak Sin-ae - Producer
+			    new { MovieId = 8, PeopleId = 40, RoleId = 4, CharacterName = (string?)null }   // Jang Young-hwan - Producer
 			);
 
             base.OnModelCreating(modelBuilder);
